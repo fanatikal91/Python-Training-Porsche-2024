a7758e6902b22ebc0874828e2df70ed36a578ce5
ahioyid <jonathan.felsing@porsche.de>
2024-11-05 13:59:09 +0100

    bugfix

--- D010_Einfaches Fenster/Main.py
+++ D010_Einfaches Fenster/Main.py
@@ -2,8 +2,8 @@ import tkinter as tk
 
 # Create the main window
 root = tk.Tk()
-root.title("Simple Tkinter Application")
-root.geometry("1200*1200")  # Set window size (width x height)
+root.title("Felsings First Application")
+root.geometry("800x800")  # Set window size (width x height)
 
 # Start the main event loop
 root.mainloop()
