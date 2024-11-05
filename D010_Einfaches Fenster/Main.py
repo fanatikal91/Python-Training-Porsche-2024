3b35ef8096f6a391075a82a8e62c0ed3b88f198e
ahioyid <jonathan.felsing@porsche.de>
2024-11-05 13:56:54 +0100

    main ergänzt

---
+++ D010_Einfaches Fenster/Main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+# Create the main window
+root = tk.Tk()
+root.title("Simple Tkinter Application")
+root.geometry("1200*1200")  # Set window size (width x height)
+
+# Start the main event loop
+root.mainloop()
